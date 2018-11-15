c53aefabf58487bfc538410ed6010701fe2405ea
Walmes Zeviani <walmes@ufpr.br>
2018-11-15 17:17:36 -0200

    Moves peaces of code to a better organization.

--- ui.R
+++ ui.R
@@ -1,169 +1,77 @@
-#---------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
+# OBS: run application on the brower to proper render CSS definitions.
 
-# OBS: RODAR NO NAVEGADOR PARA RENDERIZAR O CSS.
+#-----------------------------------------------------------------------
+# Nonlinear Regression Models Catalogue --------------------------------
 
-
-
-# Nonlinear Regression Models Catalogue -----------------------------------
-
-
-## Carregando pacotes
+#-----------------------------------------------------------------------
+# Loads packages.
 
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
 library(markdown)
 
-## Começo do codigo:
+#-----------------------------------------------------------------------
+# Components of the user interface.
 
-shinyUI(dashboardPage(
-  
-  # Title  
-  
-  title = "nlmSet",
-  
-  # Skin
-  
-  skin = "green",
-  
-  # Header
-  
-  
-  dashboardHeader(title = "nlmSet",
-                  titleWidth = 229.41,
-                  tags$li(class= "dropdown",
-                          tags$a(href= "https://github.com/willianramos12/nlmSet", 
-                                 icon("github"), 
-                                 "Source Code",
-                                 target="_blank"))
-  ),
-  
-  # Sidebar
-  
-  dashboardSidebar(
-    
-    # CSS e HTML
-    
-    tags$style(includeCSS("style.css")),
-    
-    # Abas 
-    
-    sidebarMenu(
-      
-      id = "tabs",
-      
-      # Primeira Aba       
-      
-      menuItem("Introduction",
-               tabName = "int"),
-      
-      menuItem("Models",
-               tabName = "mod",
-               
-               
-# Modelos - Abas ----------------------------------------------------------
-               
-               
-               
-# Exponencial Assintótico -------------------------------------------------
-               
-               
-               
-               
-               menuSubItem("Asymptotic Exponential",
-                           tabName = "expA"
-               ),
-               
-               
-               # Michelis-Menten ---------------------------------------------------------
-               
-               
-               
-               
-               menuSubItem("Michaelis-Menten",
-                           tabName = "MM"
-               )
-               
-               
-               # Guia de Contribuicao ----------------------------------------------------
-               
-               
-      ),#menuItem
-      
-      menuItem("Contribution",
-               tabName = "cont"
-      )
-      
-      
-      
-    ) #sidebarMenu
-  ), #dashboardSidebar
-  
-  
-  # Body --------------------------------------------------------------------
-  
-  
-  dashboardBody(
-    
-    tabItems(
-      
-      # Tela inicial      
-      
-      tabItem(tabName = "int",
-              includeMarkdown("README.md")
-      ),
-      
-      
-      
-# Modelos      
-      
-      
-      
-# Exponencial Assintótica -------------------------------------------------
-      
-      
-      tabItem(tabName = "expA",
-              
-              fluidPage(
-                
-                fluidRow(
-                  
-                  source("Models/AsymExp/uiasymexp.R", local = TRUE)$value
-                  
-                  
-                ) # fluidRow
-              )#fluidPage
-      ),#tabItem
-      
-      
-      # Michaelis-Menten --------------------------------------------------------
-      
-      
-      tabItem(tabName = "MM",
-              
-              fluidPage(
-                
-                fluidRow(
-                  
-                  source("Models/MicMen/uimicmen.R", local = TRUE)$value
-                  
-                ) # fluidRow
-              )#fluidPage
-      ),#tabItem
-      
-      
-      
-      # Guia de Contribuicao ----------------------------------------------------
-      
-      
-      tabItem(tabName = "cont",
-              includeMarkdown("contribuition.md")
-      )
-      
-      
-      #-----------------------------------------------------------------------------------
-      
-    )#tabItems  
-  )#dashboardBody
-)#dashboardPage
-)#ShinyUI
+# Header.
+db_header <-
+    dashboardHeader(
+        title = "nlmSet",
+        titleWidth = 229.41,
+        tags$li(class = "dropdown",
+                tags$a(href = "https://github.com/willianramos12/nlmSet",
+                       icon("github"),
+                       "Source Code",
+                       target = "_blank")))
+
+# Sidebar menu.
+db_sidebar <-
+    dashboardSidebar(
+        # CSS and HTML.
+        tags$style(includeCSS("style.css")),
+        # Tab panels.
+        sidebarMenu(
+            id = "tabs",
+            menuItem("Introduction", tabName = "int"),
+            menuItem("Models",
+                     tabName = "mod",
+                     menuSubItem("Asymptotic Exponential", tabName = "expA"),
+                     menuSubItem("Michaelis-Menten", tabName = "MM")
+                     ), # menuItem
+            menuItem("Contribution", tabName = "cont")
+        ) # sidebarMenu
+    )
+
+# Main area.
+db_body <-
+    dashboardBody(
+        tabItems(
+            # Tela inicial
+            tabItem(tabName = "int", includeMarkdown("README.md")),
+            # Models.
+            tabItem(tabName = "expA",
+                    fluidPage(
+                        fluidRow(
+                            source("Models/AsymExp/uiasymexp.R", local = TRUE)$value
+                        )
+                    )
+                    ),
+            tabItem(tabName = "MM",
+                    fluidPage(
+                        fluidRow(
+                            source("Models/MicMen/uimicmen.R", local = TRUE)$value
+                        )
+                    )
+                    ),
+            tabItem(tabName = "cont", includeMarkdown("contribuition.md"))
+        ) # tabItems
+    )
+
+# Call each component.
+shinyUI(dashboardPage(title = "nlmSet",
+                      skin = "green",
+                      header = db_header,
+                      sidebar = db_sidebar,
+                      body = db_body))
